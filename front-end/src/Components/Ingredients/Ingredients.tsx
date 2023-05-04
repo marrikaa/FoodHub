cb627a20d90ca0198ea85d7f10ee8085f87990ca
marrikaa <marikagachechiladze0@gmail.com>
2023-05-04 21:15:33 +0200

    instruction

--- front-end/src/Components/Ingredients/Ingredients.tsx
+++ front-end/src/Components/Ingredients/Ingredients.tsx
@@ -7,6 +7,7 @@ import './Ingredients.css'
 function Ingredients (props: Idtype)  {
     const { recipeId } = props;
     const [ingredients, setIngredients] = useState<IngredientsType[]>([]);
+    
     useEffect(()=>{
         const getIngredients = async() => {
             const ingredients = await getIngredientsByID(recipeId);
